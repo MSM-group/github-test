899775524ac30287ab434ab07f57dd3a661d000f
Serina Robinson <robinsonserinalee@gmail.com>
2022-10-17 21:40:02 +0200

    initial commit

---
+++ scripts/20221018_testscript_Robinson.r
@@ -0,0 +1,15 @@
+# Remember to open R Project or set working directory using setwd()
+
+# Read in the data
+dat <- read.csv("data/iris_petals.csv")
+petal_length <- dat$petlen
+petal_width <- dat$petwidth
+
+# Plot
+plot(petal_length, petal_width)
+
+# Write to file
+pdf("output/test_plot.pdf")
+plot(petal_length, petal_width)
+dev.off()
+
